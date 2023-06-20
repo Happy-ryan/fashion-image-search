9a078c8afce177e90781ca5b159d23d1099e5ec2
happyryan <tjdgks120813@gmail.com>
2023-06-20 12:29:16 +0900

    fix: uvicorn.run  embedding:app 수정

--- back/proxy.py
+++ back/proxy.py
@@ -13,4 +13,4 @@ async def servercheck() -> dict:
     
 
 if __name__ == "__main__":
-    uvicorn.run("embedding:app", port=8000, host="0.0.0.0", reload=False)
+    uvicorn.run("proxy:app", port=8000, host="0.0.0.0", reload=False)
