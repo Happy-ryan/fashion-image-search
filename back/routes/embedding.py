eef5c3922d3b3c94da4790f19a40e6359fa3790a
root <tjdgks120813@gmail.com>
2023-07-04 06:31:13 +0000

    feat: add text embedding model, router, client

--- back/routes/embedding.py
+++ back/routes/embedding.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+
 from infra.embedding.model import EmbeddingModel
+from models.embedding import Text
 
 embedding_router = APIRouter(
     tags=["Embedding"]
@@ -7,10 +9,20 @@ embedding_router = APIRouter(
 
 model = EmbeddingModel()
 
-@embedding_router.get("/{rid}")
-async def get_embedding(rid: str) -> dict:
+@embedding_router.get("/image/{rid}")
+async def get_image_embedding(rid: str) -> dict:
     print("rid는 여기다!:", rid)
-    embedding = model.get_embedding(rid)
+    embedding = model.get_image_embedding(rid)
+    return {
+        "msg": "OK!",
+        "embedding": embedding
+    }
+    
+# url에 query x => post => 요청바디 필요
+@embedding_router.post("/text")
+async def get_image_embedding(text: Text) -> dict:
+    print("내가 넣은 text는 여기다!:", text.text)
+    embedding = model.get_text_embedding(text.text)
     return {
         "msg": "OK!",
         "embedding": embedding
