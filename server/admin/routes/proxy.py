18be7b7efadb88cfab1d5b680ef611280731fb8d
root <tjdgks120813@gmail.com>
2023-07-12 15:54:07 +0900

    feat: admin + mongdb connection, not crud!

--- server/admin/routes/proxy.py
+++ server/admin/routes/proxy.py
@@ -1,23 +1,28 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Form
 from io import StringIO
 import pandas as pd
 
-from infra.client import DataBaseClient
+from typing import Annotated
+from dotenv import dotenv_values
+from pymongo import MongoClient
 
-from infra.connection import DB
-from models.product import Item
+from infra.client import DataBaseClient
+from .utils import makeJsonList
+
+config = dotenv_values(".env")
 
 proxy_router = APIRouter(
     tags=["Proxy"],
 )
 
 database_client = DataBaseClient()
-db_crud = DB(Item)
+
+client = MongoClient(config["ATLAS_URI"],connect=False)
+mydb = client[config["DB_NAME"]]
 
 @proxy_router.post('/products-upload')
-async def upload(file: UploadFile = File(...)):
-    print("여기는 products-upload 라우터 위치다!")
-
+async def upload(file: UploadFile = File(...), type: str = Form(...)):
+    # csv 읽기
     contents = file.file.read()
     s = str(contents,'utf-8')
     data = StringIO(s)
@@ -25,23 +30,43 @@ async def upload(file: UploadFile = File(...)):
     data.close()
     file.file.close()
     
-    keys = df['key'].to_list()[:2]
-    paths = df['path'].to_list()[:2]
-    links = df['link'].to_list()[:2]
-    names = df['name'].to_list()[:2]
+    # collection ex) Top, Bottom
+    collection = mydb[type]
+    metadata_json = makeJsonList(df, type)
+    collection.insert_many(metadata_json)
+    total_item = collection.count_documents({})
     
-    print("key는 여기다!!",  keys)
-    pickles = await database_client.make_pickle(keys, paths)
+    # key - 기본키, 중복된 키 찾기
+    duplicates = collection.aggregate([
+    {"$group": {"_id": "$key", "count": {"$sum": 1}}},
+    {"$match": {"count": {"$gt": 1}}}
+        ])
     
-    datas = []
-    for idx in range(len(keys)):
-        data = {
-            'key': keys[idx],
-            'name': names[idx],
-            'link': links[idx]
-        }
-        datas.apped(data)
+    # 중복된 키 제거
+    duplication_keys = []
+    for duplicate in duplicates:
+        key = duplicate['_id']
+        duplication_keys.append(key)
+        duplicates_to_remove = collection.find({"key": key})
+        for duplicate_to_remove in duplicates_to_remove[1:]:
+            collection.delete_one({"_id": duplicate_to_remove["_id"]})
+            
+    # 중복된 키 df에서 제거
+    df = df[~df['key'].isin(duplication_keys)]
     
-    await db_crud.insert_one(datas[0])
+    # 임베딩 저장
+    keys = df['key'].to_list()
+    paths = df['path'].to_list() # image_path 의미
     
-    return f"등록하신 상품의 개수는 {len(pickles)} 입니다."
+    if len(keys) != 0:
+        num_pickles = await database_client.make_pickle(keys, paths)
+    else:
+        num_pickles = 0
+        
+    return {
+        "등록한 총 상품의 수": total_item,
+        "이미 등록된 상품의 수": len(duplication_keys),
+        "META DB에 최종 등록된 상품의 수": total_item - len(duplication_keys),
+        "생성된 임베딩의 수": num_pickles,
+        "최종 저장된 상품의 수와 생성된 임베딩의 수의 차가 0인 경우 정상": total_item - len(duplication_keys) - num_pickles
+            }
