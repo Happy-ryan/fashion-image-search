1474a097ff5aa9c93948e7187552c429e82963d7
root <tjdgks120813@gmail.com>
2023-07-12 15:54:07 +0900

    feat: admin + mongdb connection, not crud!

--- server/admin/proxy.py
+++ server/admin/proxy.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
-from infra.connection import Setting
+
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+# config = dotenv_values(".env")
 
 app = FastAPI()
-setting = Setting()
 
 from routes.proxy import proxy_router
 
 app.include_router(proxy_router, prefix="/proxy")
+
+# client = MongoClient(config["ATLAS_URI"],connect=False)
+
 
 @app.get("/")
 async def servercheck() -> dict:
@@ -16,9 +22,16 @@ async def servercheck() -> dict:
     }
 
 
-@app.on_event("startup")
-async def init_db():
-    await setting.initialize_database()
+# @app.on_event("startup")
+# def startup_db_client():
+#     app.mongodb_client = MongoClient(config["ATLAS_URI"])
+#     app.database = app.mongodb_client[config["DB_NAME"]]
+#     print("Connected to the MongoDB database!")
+
+# @app.on_event("shutdown")
+# def shutdown_db_client():
+#     app.mongodb_client.close()
+#     print("Disconnected to the MongoDB database!")
 
 
 if __name__ == "__main__":
