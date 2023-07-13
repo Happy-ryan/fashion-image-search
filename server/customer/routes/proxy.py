03af313a45a55d3ca616633caf23cbcdf2349fa3
root <tjdgks120813@gmail.com>
2023-07-13 10:09:18 +0900

    feat: customer/proxy server connection db

--- server/customer/routes/proxy.py
+++ server/customer/routes/proxy.py
@@ -9,13 +9,19 @@ from typing import Annotated
 from fastapi import APIRouter, UploadFile, Form
 import uuid
 import os
-import httpx
+import httpx, time
 
 from infra.embedding.client import EmbeddingClient
 from infra.search.client import SearchClient
 from infra.storage.client import LocalStorageClient
 
 from models.proxy import ImageP, TextP, FilterP
+
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from pprint import pprint
+
+config = dotenv_values("/opt/ml/fashion-image-search/server/admin/.env")
 
 proxy_router = APIRouter(
     tags=["Proxy"],
@@ -24,6 +30,9 @@ proxy_router = APIRouter(
 embedding_client = EmbeddingClient()
 search_client = SearchClient()
 storage_client = LocalStorageClient("queries")
+
+client = MongoClient(config["ATLAS_URI"],connect=False)
+mydb = client[config["DB_NAME"]]
 
 @proxy_router.post("/search-by-image")
 # async def search_by_image(file: UploadFile, thresh: float) -> dict:
@@ -40,6 +49,21 @@ async def search_by_image(file: UploadFile, thresh: Annotated[float, Form()]) ->
     
     dists, ids = await search_client.search(embedding, thresh)
     
+    # fine_one의 Execution time: 3.6651980876922607 seconds > 실제 검색 시 8.37s
+    # find의 Execution time: 0.20023798942565918 seconds
+    # find + ids의 순서로 정렬한 것의 Execution time: 0.20313620567321777 seconds > 실제 검색 시 6.11s
+    # ids의 순서는 곧 유사도의 순위 >find_one이 find보다 18배나 느림 하지만 유사도 순으로 정렬되어있음.
+    # find 후 정렬을 사용하는 것이 find만 사용하는 것보다 0.0003s 차이지만 find_one보다는 압도적 성능을 지닌다.
+    # 빠른 검색이 목적: find / 유사도 정렬이 목적: find_one
+ 
+    collection = mydb["Top"]
+    documents = collection.find({'key': {'$in': ids}})
+    sorted_documents = sorted(documents, key=lambda doc: ids.index(doc['key']))
+
+    for doc in sorted_documents:
+        print(doc)
+
+    
     return {
         "msg": "OK",
         "embedding": embedding,
@@ -55,12 +79,16 @@ async def search_by_text(textp: TextP)-> dict:
     text = textp.text
     thresh = textp.thresh
     
-    print(text)
-    print(thresh)
-    
     embedding = await embedding_client.get_text_embedding(text)
     
     dists, ids = await search_client.search(embedding, thresh)
+    
+    collection = mydb["Top"]
+    documents = collection.find({'key': {'$in': ids}})
+    sorted_documents = sorted(documents, key=lambda doc: ids.index(doc['key']))
+
+    for doc in sorted_documents:
+        print(doc)
     
     return {
         "msg": "OK",
@@ -83,6 +111,15 @@ async def search_by_filter(file: UploadFile, text: Annotated[str, Form()], thres
     filter_embedding = await embedding_client.get_text_embedding(text)
     
     dists, ids = await search_client.search_with_filter(embedding, filter_embedding, thresh)
+    
+    collection = mydb["Top"]
+    documents = collection.find({'key': {'$in': ids}})
+    sorted_documents = sorted(documents, key=lambda doc: ids.index(doc['key']))
+
+    print("list", list(sorted_documents))
+    print("찾은 상품의 수", len(list(sorted_documents)))
+    # for doc in sorted_documents:
+    #     print(doc)
     
     return {
         "msg": "OK",
