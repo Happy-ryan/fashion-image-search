fbecc156b349a860167fc581c2da46ddd8eb3253
root <tjdgks120813@gmail.com>
2023-07-24 06:16:24 +0900

    re: meta & translate client

--- server/customer/routes/proxy.py
+++ server/customer/routes/proxy.py
@@ -14,12 +14,12 @@ import httpx, time
 from infra.embedding.client import EmbeddingClient
 from infra.search.client import SearchClient
 from infra.storage.client import LocalStorageClient, GCStorageClient
+from infra.meta.client import MetaClient
+from infra.translate.client import GoogleTranslateClient
 
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from pprint import pprint
-
-from .utils import objectIdDecoder, translate_text
 
 # config = dotenv_values("/opt/ml/fashion-image-search/server/admin/.env")
 import yaml
@@ -33,9 +33,9 @@ proxy_router = APIRouter(
 embedding_client = EmbeddingClient()
 search_client = SearchClient()
 storage_client = LocalStorageClient("queries", config_path="../config.yaml")
+meta_client = MetaClient("products", config_path="../config.yaml")
+translate_client = GoogleTranslateClient(config_path="../config.yaml")
 
-client = MongoClient(config["mongodb"]["url"],connect=False)
-mydb = client[config["mongodb"]["name"]]
 
 @proxy_router.post("/search-by-image")
 # async def search_by_image(file: UploadFile, thresh: float) -> dict:
@@ -52,28 +52,14 @@ async def search_by_image(file: Annotated[UploadFile, File()], thresh: Annotated
     
     dists, ids = await search_client.search(embedding, thresh)
     
-    # fine_one의 Execution time: 3.6651980876922607 seconds > 실제 검색 시 8.37s
-    # find의 Execution time: 0.20023798942565918 seconds
-    # find + ids의 순서로 정렬한 것의 Execution time: 0.20313620567321777 seconds > 실제 검색 시 6.11s
-    # ids의 순서는 곧 유사도의 순위 >find_one이 find보다 18배나 느림 하지만 유사도 순으로 정렬되어있음.
-    # find 후 정렬을 사용하는 것이 find만 사용하는 것보다 0.0003s 차이지만 find_one보다는 압도적 성능을 지닌다.
-    # 빠른 검색이 목적: find / 유사도 정렬이 목적: find_one
- 
-    collection = mydb[config["mongodb"]["collection"]]
-    documents = collection.find({'key': {'$in': ids}})
-    sorted_documents = sorted(documents, key=lambda doc: ids.index(doc['key']))
+    documents = meta_client.find(ids)
 
-    # product_list = []
-    # for doc in sorted_documents:
-    #     print(doc['meta'])
-
-    # print("여기를 살펴보자!", sorted_documents)
     return {
         # "msg": "OK",
         # "embedding": embedding,
         # "dists": dists,
         # "ids": ids,
-        "상품목록": objectIdDecoder(sorted_documents)
+        "상품목록": documents,
     }
     
 
@@ -81,26 +67,21 @@ async def search_by_image(file: Annotated[UploadFile, File()], thresh: Annotated
 async def search_by_text(text: Annotated[str, Form()], thresh: Annotated[float, Form()])-> dict:
     print("proxy server 여기까지는 왔니? - 2")
     
-    text = translate_text(text)
+    text = await translate_client.translate(text)
     
     embedding = await embedding_client.get_text_embedding(text)
     
     dists, ids = await search_client.search(embedding, thresh)
     
-    collection = mydb[config["mongodb"]["collection"]]
-    documents = collection.find({'key': {'$in': ids}})
-    sorted_documents = sorted(documents, key=lambda doc: ids.index(doc['key']))
+    documents = meta_client.find(ids)
 
-    # for doc in sorted_documents:
-    #     print(doc)
-    
     return {
         # "msg": "OK",
         # "embedding": embedding,
         # "dists": dists,
         # "ids": ids,
         "번역 결과": text,
-        "상품목록": objectIdDecoder(sorted_documents)
+        "상품목록": documents,
     }
     
 
@@ -113,30 +94,21 @@ async def search_by_filter(file: UploadFile, text: Annotated[str, Form()], thres
     filename = f"{rid}.png"
     storage_client.save(filename, content)
     
-    text = translate_text(text)
-        
+    text = await translate_client.translate(text)
+
     embedding = await embedding_client.get_image_embedding(rid)
     
     filter_embedding = await embedding_client.get_text_embedding(text)
     
     dists, ids = await search_client.search_with_filter(embedding, filter_embedding, thresh)
     
-    collection = mydb[config["mongodb"]["collection"]]
-    documents = collection.find({'key': {'$in': ids}})
-    sorted_documents = sorted(documents, key=lambda doc: ids.index(doc['key']))
+    documents = meta_client.find(ids)
 
-    # print("list", type(sorted_documents))
-    # print("찾은 상품의 수", len(list(sorted_documents)))
-    # print("첫 번째", sorted_documents[0])
-    # print("뭐가 나올까요?", objectIdDecoder(sorted_documents))
-    # for doc in sorted_documents:
-    #     print(doc)
-  
     return {
         # "msg": "OK",
         # "embedding": embedding,
         # "dists": dists,
         # "ids": ids,
         "번역 결과": text,
-        "상품목록": objectIdDecoder(sorted_documents)
+        "상품목록": documents,
     }
