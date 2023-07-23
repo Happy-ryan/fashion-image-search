56a830761ac883549419e5ab130eaa3fbbf0eaa4
root <tjdgks120813@gmail.com>
2023-07-24 06:07:51 +0900

    feat: add process time middleware & storage mount

--- server/customer/search.py
+++ server/customer/search.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
+import time
 
 app = FastAPI()
 
 from routes.search import search_router
 
 app.include_router(search_router, prefix="/search")
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    elapsed_time = time.time() - start_time
+    print(f"response time: {elapsed_time:.3f} sec")
+    return response
+
 
 @app.get("/")
 async def servercheck() -> dict:
